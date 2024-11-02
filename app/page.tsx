6a060aa421e850fd6f7c802345ff1904b56501ba
Manuel Garcia <ymelicrow@gmail.com>
2024-11-02 19:16:20 -0300

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,54 @@
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div className="text-white ">
+            <img src="bg.png" className="absolute left-1/2 transform -translate-x-1/2 -z-10" alt="" />
+            <div className="absolute size-32 bg-[#1DF573] blur-[170px] "></div>
+            <header className="w-screen flex justify-between p-12 ">
+                <img src="logo.png" alt="" />
+                <nav className="text-white cursor-pointer z-20">
+                    <ul className="flex gap-7 text-xl">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#" className="opacity-45 hover:opacity-100 transition 0.2s all ease-out">Loja</a></li>
+                        <li><a href="#" className="opacity-45 hover:opacity-100 transition 0.2s all ease-out">Termos</a></li>
+                        <li><a href="#" className="opacity-45 hover:opacity-100 transition 0.2s all ease-out">Regras</a></li>    
+                    </ul>
+                </nav>
+                <div>
+                    <div className="absolute size-32 bg-[#1DF573] blur-[170px] -z-10"></div>
+                    <a href="#" className=" rounded-full bg-[#1DF573] text-[#0B0A0D] px-11 py-4 hover:bg-[#3cbb6f] transition 0.2s all ease-out">Login</a>
+                </div>
+            </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <main className="flex flex-col justify-center items-center gap-6 mt-40">
+                <h1 className="font-bold text-5xl text-center">Seja <span className="text-[#1DF573]">bem vindo</span> ao WebSite do <br /> <span className="text-[#1DF573]">Vanilla Roleplay </span></h1>
+                <h2 className="font-normal text-lg text-center opacity-45">No Vanilla, você não só joga, você vive. Aqui, cada personagem é uma nova história <br /> esperando para ser contada. Seja um trabalhador comum, um empresário de sucesso, ou <br /> alguém mais ousado, o servidor te dá a liberdade para criar e evoluir do seu jeito.</h2>
+                <a href="" className=" rounded-full bg-[#1DF573] text-[#0B0A0D] px-11 py-4 font-bold hover:bg-[#3cbb6f] transition 0.2s all ease-out">Entrar no Servidor</a>
+                <div className="left-full bottom-10 absolute size-32 bg-[#1DF573] blur-[170px] -z-10"></div>
+            </main>
+            <section className="h-screen">
+                <div className=" absolute size-32 bg-[#1DF573] blur-[170px]"></div>
+            </section>
+
+            <footer className="absolute  w-full h-[267px] bg-[#0C100E] ">
+                <div className="h-[267px] flex justify-between items-center px-10">
+                    <div className="flex flex-col gap-4">
+                        <img src="logo-footer.png" className="w-[200px]" alt="" />
+                        <div>
+                            <h1 className="font-bold">Vanilla Roleplay 2021 - 2024</h1>
+                            <h2 className="opacity-45">© Todos os direitos reservados</h2>
+                        </div>
+                        <p className="opacity-45">O Vanilla Roleplay não é afiliado a Rockstar Games Inc.</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex justify-end gap-4">
+                            <a href=""><img src="discord.svg" className="p-5 bg-white rounded-full" alt="" /></a>
+                            <a href=""><img src="youtube.svg" className="p-5 bg-white rounded-full" alt="" /></a>
+                        </div>
+                        <h1>Desenvolvido com carinho por <strong className="text-[#1DF573]">imSand</strong>.</h1>
+                    </div>
+                </div>
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
